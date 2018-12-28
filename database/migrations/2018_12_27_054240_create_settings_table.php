ab2b9342c46b24e125983aa20c3c60e0e18487a6
ankittatrasdata <ankit@tatrasdata.com>
2018-12-28 16:42:34 +0530

    simple curl

--- database/migrations/2018_12_27_054240_create_settings_table.php
+++ database/migrations/2018_12_27_054240_create_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateSettingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('settings', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('role_id')->default(4);
+            $table->datetime('nomination_start_date');
+            $table->datetime('nomination_end_date');
+            $table->datetime('voting_start_date');
+            $table->datetime('voting_end_date');
             $table->softDeletes();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('settings');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'ProductController@index')->name('home');
+
+Route::resource('products','ProductController')->middleware('auth');
+
